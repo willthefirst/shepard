82fa93bd3072a72d0630b28afce8b152413ab9d3
Will Moritz <will@crowdfavorite.com>
2020-11-17 07:59:14 -0800

    basic tone

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("hasd")
+//create a synth and connect it to the main output (your speakers)
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+
+const playButton = document.getElementById("play-button");
+
+playButton.addEventListener('click', () => {
+    synth.triggerAttackRelease("C4", "8n");
+});
