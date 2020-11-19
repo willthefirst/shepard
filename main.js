5fa14071e1df5c34729acac6986df314e7943a7c
Will Moritz <will@crowdfavorite.com>
2020-11-18 19:28:57 -0800

    this seems to be working

--- main.js
+++ main.js
@@ -1,51 +1,30 @@
-//create a synth and connect it to the main output (your speakers)
-const gainNode1 = new Tone.Gain(0).toDestination();
-const osc1 = new Tone.Oscillator().connect(gainNode1);
-
-const gainNode2 = new Tone.Gain(1).toDestination();
-const osc2 = new Tone.Oscillator().connect(gainNode2);
-
-const gainNode3 = new Tone.Gain(1).toDestination();
-const osc3 = new Tone.Oscillator().connect(gainNode3);
-
-
-
 const playButton = document.getElementById("play-button");
 
-playButton.addEventListener("click", () => {
-    const interval = 2;
-    
-    // Need this legally initialize audio
-	Tone.start();
+playButton.addEventListener("click", async () => {
+	const interval = 2;
 
-    //play a note every eighth note starting from the first measure
-    // Tone.Transport.scheduleRepeat(function(time){
-    //     note.triggerAttack(time);
-    // }, "8n", "1m");
+	// Need this legally initialize audio
+	await Tone.start();
+	console.log("audio is ready");
 
+	//play a note every eighth note starting from the first measure
+	// Tone.Transport.scheduleRepeat(function(time){
+	//     note.triggerAttack(time);
+	// }, "8n", "1m");
+
+	const gainNode1 = new Tone.Gain(0).toDestination();
+	const osc1 = new Tone.Oscillator("C5").connect(gainNode1);
 	const high = new Tone.Loop((time) => {
-        osc1.frequency.value = "C6";
-        gainNode1.gain.value = 0;
+		osc1.start();
 
-        osc1.frequency.linearRampToValueAtTime("C5", time + interval);
+		console.log(osc1.frequency.value);
 
-        gainNode1.gain.rampTo(1, interval/2);
-        gainNode1.gain.rampTo(0, interval/2, time + interval/2);
+		osc1.frequency.rampTo("C5", Tone.now);
+		osc1.frequency.rampTo("C4", "1n");
 
-        osc1.start();
-    }, interval).start();
-     
-    const low = new Tone.Loop((time) => {
-        osc2.frequency.value = "C5";
-        gainNode2.gain.value = 1;
-
-        osc2.frequency.linearRampToValueAtTime("C4", time + interval);
-
-        gainNode2.gain.rampTo(0, interval/2);
-        gainNode2.gain.rampTo(1, interval/2, time + interval/2);
-
-        osc2.start();
-    }, interval).start();
+		gainNode1.gain.rampTo(1, "2n");
+		gainNode1.gain.rampTo(0, "2n", "+2n");
+	}, "1n").start(0);
 
 	Tone.Transport.start();
 });
